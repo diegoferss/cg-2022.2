b41686c31322f122df423fd8f46352910ec0ea25
Thiago Ferronatto <thiagoferronatto18@gmail.com>
2022-11-16 07:29:45 -0400

    [NEW] File save dialog implemented.
    The "Export Wavefront" option under the Tools menu currently saves the default spiral to a Wavefront OBJ format.
    
    TODO: save not the default spiral, but the highlighted mesh if there is one.
    
    Changes:
        [ADDED] nfd.h: Header for the NFD static library.
        [ADDED] nfd.lib: The NFD static library.
        [ADDED] nfd_d.lib: Debug version of nfd.lib.
        [MOD] generatrix.cpp: Applied the Google C++ formatting standard;
        [MOD] generatrix.h: Applied the Google C++ formatting standard;
        [MOD] obj_writer.cpp: Added nullity check for a FILE*.

--- apps/tp/MainWindow.cpp
+++ apps/tp/MainWindow.cpp
@@ -30,11 +30,25 @@
 // Author: Paulo Pagliosa
 // Last revision: 11/03/2022
 
-#include "graphics/Application.h"
-#include "reader/SceneReader.h"
 #include "MainWindow.h"
-#include "our_GLGraphics.h"
 
+#include <cstdio>
+
+#include "graphics/Application.h"
+#include "nfd.h"
+#include "obj_writer.h"
+#include "our_GLGraphics.h"
+#include "reader/SceneReader.h"
+
+#ifdef _DEBUG
+#define NFD_CHECK(result)                                   \
+  {                                                         \
+    if ((result) == NFD_ERROR)                              \
+      fprintf_s(stderr, "NFD Error: %s\n", NFD_GetError()); \
+  }
+#else
+#define NFD_CHECK(result) (result)
+#endif  // _DEBUG
 
 /////////////////////////////////////////////////////////////////////
 //
@@ -42,9 +56,7 @@
 // ==========
 MeshMap MainWindow::_defaultMeshes;
 
-inline void
-MainWindow::buildDefaultMeshes()
-{
+inline void MainWindow::buildDefaultMeshes() {
   _defaultMeshes["Box"] = GLGraphics3::box();
   _defaultMeshes["Sphere"] = GLGraphics3::sphere();
   _defaultMeshes["Cylinder"] = GLGraphics3::cylinder();
@@ -53,161 +65,115 @@ MainWindow::buildDefaultMeshes()
   _defaultMeshes["Twist"] = OurGLGraphics::twist();
 }
 
-void
-MainWindow::initializeScene()
-{
+void MainWindow::initializeScene() {
   auto a = (float)width() / (float)height();
   auto s = createCameraObject(a, "Main Camera");
 
   s->transform()->setLocalPosition({0, 0, 10});
   s = createLightObject(Light::Type::Directional, "Directional Light");
   s->transform()->setLocalEulerAngles({50, 30, 0});
-  //createDefaultPrimitiveObject("Spiral");
+  // createDefaultPrimitiveObject("Spiral");
 }
 
-void
-MainWindow::beginInitialize()
-{
+void MainWindow::beginInitialize() {
   Assets::initialize();
   buildDefaultMeshes();
   Assets::meshes().insert(_defaultMeshes.begin(), _defaultMeshes.end());
 
-  constexpr auto ffn = "fonts/Roboto-Regular.ttf";
+  /*constexpr auto ffn = "fonts/Roboto-Regular.ttf";
   auto fonts = ImGui::GetIO().Fonts;
 
-  fonts->AddFontFromFileTTF(Application::assetFilePath(ffn).c_str(), 16);
+  fonts->AddFontFromFileTTF(Application::assetFilePath(ffn).c_str(), 16);*/
 }
 
-void
-MainWindow::createObjectMenu()
-{
-  if (ImGui::BeginMenu("3D Object"))
-  {
-    if (ImGui::MenuItem("Box"))
-      createDefaultPrimitiveObject("Box");
-    if (ImGui::MenuItem("Sphere"))
-      createDefaultPrimitiveObject("Sphere");
-    if (ImGui::MenuItem("Cylinder"))
-      createDefaultPrimitiveObject("Cylinder");
-    if (ImGui::MenuItem("Cone"))
-      createDefaultPrimitiveObject("Cone");
-    if (ImGui::MenuItem("Spiral"))
-        createDefaultPrimitiveObject("Spiral");
-    if (ImGui::MenuItem("Twist"))
-        createDefaultPrimitiveObject("Twist");
+void MainWindow::createObjectMenu() {
+  if (ImGui::BeginMenu("3D Object")) {
+    if (ImGui::MenuItem("Box")) createDefaultPrimitiveObject("Box");
+    if (ImGui::MenuItem("Sphere")) createDefaultPrimitiveObject("Sphere");
+    if (ImGui::MenuItem("Cylinder")) createDefaultPrimitiveObject("Cylinder");
+    if (ImGui::MenuItem("Cone")) createDefaultPrimitiveObject("Cone");
+    if (ImGui::MenuItem("Spiral")) createDefaultPrimitiveObject("Spiral");
+    if (ImGui::MenuItem("Twist")) createDefaultPrimitiveObject("Twist");
     ImGui::EndMenu();
   }
 }
 
-Component*
-MainWindow::addComponentMenu()
-{
+Component* MainWindow::addComponentMenu() {
   Component* component = nullptr;
 
-  if (ImGui::BeginMenu("3D Object"))
-  {
-    if (ImGui::MenuItem("Box"))
-      component = makeDefaultPrimitive("Box");
-    if (ImGui::MenuItem("Sphere"))
-      component = makeDefaultPrimitive("Sphere");
+  if (ImGui::BeginMenu("3D Object")) {
+    if (ImGui::MenuItem("Box")) component = makeDefaultPrimitive("Box");
+    if (ImGui::MenuItem("Sphere")) component = makeDefaultPrimitive("Sphere");
     if (ImGui::MenuItem("Cylinder"))
       component = makeDefaultPrimitive("Cylinder");
-    if (ImGui::MenuItem("Cone"))
-      component = makeDefaultPrimitive("Cone");
-    if (ImGui::MenuItem("Spiral"))
-      component = makeDefaultPrimitive("Spiral");
-    if (ImGui::MenuItem("Plane"))
-      component = makeDefaultPrimitive("Plane");
+    if (ImGui::MenuItem("Cone")) component = makeDefaultPrimitive("Cone");
+    if (ImGui::MenuItem("Spiral")) component = makeDefaultPrimitive("Spiral");
+    if (ImGui::MenuItem("Plane")) component = makeDefaultPrimitive("Plane");
     ImGui::EndMenu();
   }
   return component;
 }
 
-void
-MainWindow::readScene(const std::string& filename)
-{
-  try
-  {
+void MainWindow::readScene(const std::string& filename) {
+  try {
     parser::SceneReader reader;
 
     reader.setInput(filename);
     reader.execute();
-    if (reader.scene() != nullptr)
-      SceneWindow::setScene(*reader.scene());
-  }
-  catch (const std::exception& e)
-  {
+    if (reader.scene() != nullptr) SceneWindow::setScene(*reader.scene());
+  } catch (const std::exception& e) {
     puts(e.what());
   }
 }
 
-inline void
-MainWindow::fileMenu()
-{
-  if (ImGui::BeginMenu("File"))
-  {
-    if (ImGui::MenuItem("New Scene"))
-      newScene();
-    if (ImGui::BeginMenu("Open"))
-    {
+inline void MainWindow::fileMenu() {
+  if (ImGui::BeginMenu("File")) {
+    if (ImGui::MenuItem("New Scene")) newScene();
+    if (ImGui::BeginMenu("Open")) {
       // TODO
       if (ImGui::MenuItem("test.scn"))
         readScene(Application::assetFilePath("scenes/test.scn"));
       ImGui::EndMenu();
     }
     ImGui::Separator();
-    if (ImGui::MenuItem("Exit", "Alt+F4"))
-    {
+    if (ImGui::MenuItem("Exit", "Alt+F4")) {
       shutdown();
     }
     ImGui::EndMenu();
   }
 }
 
-inline void
-MainWindow::createMenu()
-{
-  if (ImGui::BeginMenu("Create"))
-  {
-    if (ImGui::MenuItem("Material"))
-      createMaterial();
+inline void MainWindow::createMenu() {
+  if (ImGui::BeginMenu("Create")) {
+    if (ImGui::MenuItem("Material")) createMaterial();
     ImGui::Separator();
     createObjectMenu();
     ImGui::EndMenu();
   }
 }
 
-inline void
-MainWindow::showOptions()
-{
+inline void MainWindow::showOptions() {
   ImGui::PushItemWidth(ImGui::GetWindowWidth() * 0.6f);
   ImGui::showStyleSelector("Color Theme##Selector");
   ImGui::colorEdit3("Selected Wireframe", _selectedWireframeColor);
   ImGui::PopItemWidth();
 }
 
-inline void
-MainWindow::mainMenu()
-{
-  if (ImGui::BeginMainMenuBar())
-  {
+inline void MainWindow::mainMenu() {
+  if (ImGui::BeginMainMenuBar()) {
     fileMenu();
     createMenu();
-    if (ImGui::BeginMenu("View"))
-    {
+    if (ImGui::BeginMenu("View")) {
       static const char* viewLabels[]{"Editor", "Ray Tracer"};
 
-      if (ImGui::BeginCombo("View", viewLabels[(int)_viewMode]))
-      {
-        for (auto i = 0; i < IM_ARRAYSIZE(viewLabels); ++i)
-        {
+      if (ImGui::BeginCombo("View", viewLabels[(int)_viewMode])) {
+        for (auto i = 0; i < IM_ARRAYSIZE(viewLabels); ++i) {
           if (ImGui::Selectable(viewLabels[i], _viewMode == (ViewMode)i))
             _viewMode = (ViewMode)i;
         }
         ImGui::EndCombo();
         // TODO: change mode only if scene has changed
-        if (_viewMode == ViewMode::Editor)
-          _image = nullptr;
+        if (_viewMode == ViewMode::Editor) _image = nullptr;
       }
       ImGui::Separator();
       ImGui::MenuItem("Hierarchy Window", nullptr, &_showHierarchy);
@@ -217,24 +183,31 @@ MainWindow::mainMenu()
       ImGui::MenuItem("Editor View Settings", nullptr, &_showEditorView);
       ImGui::EndMenu();
     }
-    if (ImGui::BeginMenu("Ray Tracing"))
-    {
-      ImGui::DragInt("Max Recursion Level",
-        &_maxRecursionLevel,
-        1.0f,
-        0,
-        RayTracer::maxMaxRecursionLevel);
-      ImGui::DragFloat("Min Weight",
-        &_minWeight,
-        0.01f,
-        RayTracer::minMinWeight,
-        1.0f);
+    if (ImGui::BeginMenu("Ray Tracing")) {
+      ImGui::DragInt("Max Recursion Level", &_maxRecursionLevel, 1.0f, 0,
+                     RayTracer::maxMaxRecursionLevel);
+      ImGui::DragFloat("Min Weight", &_minWeight, 0.01f,
+                       RayTracer::minMinWeight, 1.0f);
       ImGui::EndMenu();
     }
-    if (ImGui::BeginMenu("Tools"))
-    {
-      if (ImGui::BeginMenu("Options"))
-      {
+    if (ImGui::BeginMenu("Tools")) {
+      if (ImGui::MenuItem("Export Wavefront")) {
+        char* usrprof_buf;
+        _dupenv_s(&usrprof_buf, nullptr, "userprofile");
+        nfdchar_t default_path[256];
+        sprintf_s(default_path, "%s\\Desktop", usrprof_buf);
+        free(usrprof_buf);
+        nfdchar_t* out_path{};
+        NFD_CHECK(NFD_SaveDialog(nullptr, default_path, &out_path));
+        if (out_path && *out_path) {
+          std::FILE* out_handle{};
+          fopen_s(&out_handle, out_path, "w");
+          free(out_path);
+          WriteMesh(_defaultMeshes["Spiral"], out_handle);
+          if (out_handle) fclose(out_handle);
+        }
+      }
+      if (ImGui::BeginMenu("Options")) {
         showOptions();
         ImGui::EndMenu();
       }
@@ -244,12 +217,9 @@ MainWindow::mainMenu()
   }
 }
 
-void
-MainWindow::gui()
-{
+void MainWindow::gui() {
   mainMenu();
-  if (_viewMode == ViewMode::Renderer)
-    return;
+  if (_viewMode == ViewMode::Renderer) return;
 
   constexpr auto evw = (float)360;
   constexpr auto evh = (float)308;
@@ -283,18 +253,13 @@ MainWindow::gui()
   assetsWindow();
 }
 
-void
-MainWindow::renderScene()
-{
-  if (_viewMode != ViewMode::Renderer)
-    return;
+void MainWindow::renderScene() {
+  if (_viewMode != ViewMode::Renderer) return;
 
   auto camera = CameraProxy::current();
 
-  if (nullptr == camera)
-    camera = editor()->camera();
-  if (_image == nullptr)
-  {
+  if (nullptr == camera) camera = editor()->camera();
+  if (_image == nullptr) {
     _image = new GLImage{width(), height()};
     if (_rayTracer == nullptr)
       _rayTracer = new RayTracer{*scene(), *camera};
@@ -307,10 +272,12 @@ MainWindow::renderScene()
   _image->draw(0, 0);
 }
 
-bool
-MainWindow::onResize(int width, int height)
-{
+bool MainWindow::onResize(int width, int height) {
   _viewMode = ViewMode::Editor;
   _image = nullptr;
   return true;
 }
+
+#ifdef NFD_CHECK
+#undef NFD_CHECK
+#endif  // NFD_CHECK
--- apps/tp/generatrix.h
+++ apps/tp/generatrix.h
@@ -6,44 +6,44 @@
 
 namespace cg {
 
-	class Generatrix {
-	public:
-		Generatrix(int = 3);
+class Generatrix {
+ public:
+  Generatrix(int = 3);
 
-		// Sem cópias implícitas
-		Generatrix(const Generatrix&) = delete;
-		Generatrix& operator=(const Generatrix&) = delete;
+  // Sem cópias implícitas
+  Generatrix(const Generatrix&) = delete;
+  Generatrix& operator=(const Generatrix&) = delete;
 
-		// Só copiar explicitamente, pra minha sanidade
-		virtual Generatrix Copy(const Generatrix&);
+  // Só copiar explicitamente, pra minha sanidade
+  virtual Generatrix Copy(const Generatrix&);
 
-		// Mover a gnt deixa
-		Generatrix(Generatrix&&) noexcept;
-		virtual Generatrix& operator=(Generatrix&&) noexcept;
+  // Mover a gnt deixa
+  Generatrix(Generatrix&&) noexcept;
+  virtual Generatrix& operator=(Generatrix&&) noexcept;
 
-		~Generatrix();
+  ~Generatrix();
 
-		Generatrix& Translate(const vec3f&);
-		Generatrix& Rotate(float, const vec3f & = { 0, 1, 0 });
-		Generatrix& RotateInPlace(float, const vec3f & = { 0, 1, 0 });
-		Generatrix& Scale(float);
-		Generatrix& TwistScale(float);
+  Generatrix& Translate(const vec3f&);
+  Generatrix& Rotate(float, const vec3f& = {0, 1, 0});
+  Generatrix& RotateInPlace(float, const vec3f& = {0, 1, 0});
+  Generatrix& Scale(float);
+  Generatrix& TwistScale(float);
 
-		virtual void Draw(GLGraphics3*, bool = false) const;
+  virtual void Draw(GLGraphics3*, bool = false) const;
 
-		const vec3f& Center() const;
-		float Radius() const;
-		int Sides() const;
+  const vec3f& Center() const;
+  float Radius() const;
+  int Sides() const;
 
-		vec3f& operator[](int);
-		const vec3f& operator[](int) const;
+  vec3f& operator[](int);
+  const vec3f& operator[](int) const;
 
-	protected:
-		vec3f* points_;
-		vec3f center_;
-		float radius_;
-		int num_points_;
-	};
+ protected:
+  vec3f* points_;
+  vec3f center_;
+  float radius_;
+  int num_points_;
+};
 
 }  // namespace cg
 
